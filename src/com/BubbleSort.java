ed2438425ab0cf7a3ac842f0171b2777794a55ac
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2019-01-06 08:56:55 -0500

    Update BubbleSort.java

--- src/com/BubbleSort.java
+++ src/com/BubbleSort.java
@@ -1,19 +1,17 @@
 package com;
 
-public static void BubbleSort(int[] arr) {
-int x = 0;
-int y = 1;
-
-while (Arr [x] >= Arr[y]){
-    public static swap(int[]arr,int x, int y  );
-int z = arr[x]
-arr[x] = arr[y]
-arr[y] = int z    
-
-x++;
-y++;
-}
-
-
-
-}
+class BubbleSort
+{
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
